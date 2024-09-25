01095d1af09a8d21318802d376ba4fb8e4a4cacc
zhtcl <110948436+zhtcl@users.noreply.github.com>
2024-09-25 15:32:38 +0800

    Update immortalwrt.sh

--- sh/immortalwrt.sh
+++ sh/immortalwrt.sh
@@ -6,10 +6,10 @@ rm -rf feeds/packages/net/aria2
 rm -rf feeds/packages/net/ariang
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
+#rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-msd_lite
-rm -rf feeds/luci/applications/luci-app-smartdns
+#rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-netgear
 rm -rf feeds/luci/applications/luci-app-netdata
